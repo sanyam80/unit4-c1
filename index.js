b5a394802b21c5ae0bd71f2b97c973c2f4fcc765
sanyam80 <86406962+sanyam80@users.noreply.github.com>
2022-03-07 12:34:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+app.get("/books", logger,(req,res) => {
+ return res.send({route:"/books"})
+})
+app.get("/libraries",logger,  (req,res) => {
+ return res.send({route:"/libraries",permission:"true"})
+})
+app.get("/authors", logger, (req,res) => {
+    return res.send({route:"/authors",permission:"true"})
+})
+
+function logger(req,res,next){
+    console.log("request path")
+    next();
+}
+
+ function checkPermission(req,res,next){
+    if(req.path=="/libraries"){
+       req.role = "true";
+       next();
+    }
+    else if(req.path=="/authors"){
+       req.role = "true";
+       next();
+        
+    }
+    else{
+        req.role = "false"
+        next();
+    }
+    
+  
+
+}
+app.listen(5000,() =>{
+    console.log("listening on port 5000")
+})
